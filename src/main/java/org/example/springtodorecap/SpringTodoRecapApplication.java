9f37dc4028fd0c4e5fce50e3637de32b2d5c9ff1
bjoern <b.irion@wtnet.de>
2026-05-28 10:00:13 +0200

    Initial commit

---
+++ src/main/java/org/example/springtodorecap/SpringTodoRecapApplication.java
@@ -0,0 +1,13 @@
+package org.example.springtodorecap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTodoRecapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTodoRecapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springtodorecap/SpringTodoRecapApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springtodorecap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTodoRecapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
